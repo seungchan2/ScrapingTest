f63efdc59bbba461e47c4d6bd46b2509eedeab5e
chansenbbai <chanchu@hansung.ac.kr>
2022-07-26 21:04:56 +0900

    initial commit

---
+++ ScrapingTest/ScrapingTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScrapingTest
+//
+//  Created by 김승찬 on 2022/07/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
